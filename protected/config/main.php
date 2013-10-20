fadcf91d137800efe9e021a63c30feb218e1dd64
ali asif <aliasif_bce@yahoo.com>
2013-10-20 16:12:50 +0500

    -Completed Navigation -added ui boxer for selecting space

--- protected/config/main.php
+++ protected/config/main.php
@@ -18,8 +18,8 @@ return array(
                 'application.models.*',
                 'application.components.*',
         ),
-        'theme' => 'hebo',
-        
+        //'theme' => 'hebo',
+        'theme' => 'largewall',
 
         'modules'=>array(
                 // uncomment the following to enable the Gii tool
